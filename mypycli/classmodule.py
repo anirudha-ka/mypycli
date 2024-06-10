31407913bb7eff0a73c07fad3d2fbf6ac628873a
Anirudha Kanakatte <anirudha@kanakatte.com>
2024-06-10 20:37:24 +0100

    Add classmodule and funcmodule

---
+++ mypycli/classmodule.py
@@ -0,0 +1,6 @@
+class MyClass():
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def say_thanks(self):
+        print(f"Thank you, {self.name}!")
---
+++ mypycli/funcmodule.py
@@ -0,0 +1,2 @@
+def my_function(text_to_display: str):
+    print(f"Text displayed from my_function :: {text_to_display}")
