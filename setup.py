af96790874a41c114c6baea1023c929f6ddcf6b1
Anirudha Kanakatte <anirudha@kanakatte.com>
2024-06-26 20:37:59 +0100

    Add appname variable

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from mypycli import __app_name__, __version__
 setup(
     name=__app_name__ if __app_name__ else "mypycli",
     version=__version__ if __version__ else "0.2.0",
-    packages=["mypycli"],
+    packages=[f"{__app_name__}"],
     entry_points={
         "console_scripts": [
-            "mypycli=mypycli.__main__:main"
+            f"{__app_name__}={__app_name__}.__main__:main"
         ]
     }
 )
