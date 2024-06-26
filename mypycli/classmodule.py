9c594805dc10c40fb4a27530466758e137250652
Anirudha Kanakatte <anirudha@kanakatte.com>
2024-06-26 19:18:19 +0100

    Delete unused modules

--- mypycli/classmodule.py
+++
@@ -1,6 +0,0 @@
-class MyClass():
-    def __init__(self, name) -> None:
-        self.name = name
-
-    def say_thanks(self):
-        print(f"Thank you, {self.name}!")
--- mypycli/funcmodule.py
+++
@@ -1,2 +0,0 @@
-def my_function(text_to_display: str):
-    print(f"Text displayed from my_function :: {text_to_display}")
